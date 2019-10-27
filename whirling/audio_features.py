985c0c0d66c8871d9c8b0d094d85239255c03699
nates linux desktop <nhapeman@gmail.com>
2019-10-27 14:21:27 -0700

    Move some functions around. Start rms energy calculation

--- whirling/audio_features.py
+++ whirling/audio_features.py
@@ -4,7 +4,12 @@ import time
 import librosa
 import logging
 
-version = '0.0.1'
+version = '0.0.2'
+
+
+###############################################################################
+# Helpers.
+###############################################################################
 
 def timeit(method):
     def timed(*args, **kwargs):
@@ -16,13 +21,26 @@ def timeit(method):
         return result
     return timed
 
+def get_events_at_time(current_track_audio_features, curr_time):
+    beats = current_track_audio_features['beats']
+    # See what beats should be played at curr_time
+    return [b for b in beats if curr_time >= b[0] and curr_time < b[1]]
+
+
+###############################################################################
+# Loading Saving etc.
+###############################################################################
+
+def load_track(new_track: str):
+    y, sr = librosa.load(new_track)
+    return y, sr
+
 def audio_to_feature_file(track: str):
     return os.path.splitext(track)[0] + '.json'
 
 def cache_exists(track: str):
     return os.path.exists(audio_to_feature_file(track))
 
-@timeit
 def load_features(track: str):
     logging.info('Loading cached features for track: %s' % track)
     if not cache_exists(track):
@@ -36,31 +54,36 @@ def save_features(track: str, data):
     with open(feature_file, 'w') as f:
         json.dump(data, f, indent=4)
 
+
+###############################################################################
+# Feature extracting.
+###############################################################################
+
 @timeit
 def generate_features(track: str):
     logging.info('Generating features for track: %s' % track)
     y, sr = load_track(track)
-    beats = parse_beats(y, sr)
+    beats = get_beats(y, sr)
+    volume_levels = get_volume_levels(y, sr)
     data = {
         'version': version,
         'beats': beats,
+        #'volume_levels': volume_levels
     }
     save_features(track, data)
     return data
 
-def load_track(new_track: str):
-    y, sr = librosa.load(new_track)
-    return y, sr
+@timeit
+def get_volume_levels(y, sr):
+    volume_frames = librosa.feature.rms(y=y)
+    volume_levels = librosa.frames_to_time(volume_frames, sr=sr)
+    return volume_levels
 
-def parse_beats(y, sr):
+@timeit
+def get_beats(y, sr):
     tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
     logging.info('Estimated tempo: {:.2f} beats per minute'.format(tempo))
     # 4. Convert the frame indices of beat events into timestamps
     beat_times = librosa.frames_to_time(beat_frames, sr=sr)
     sustain = 0.2
     return [(b, b + sustain) for b in beat_times]
-
-def get_events_at_time(current_track_audio_features, curr_time):
-    beats = current_track_audio_features['beats']
-    # See what beats should be played at curr_time
-    return [b for b in beats if curr_time >= b[0] and curr_time < b[1]]
