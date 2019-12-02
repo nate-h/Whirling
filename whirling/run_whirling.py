2bd6912b32629c67ec9012d5620f919ba0a327a3
nates linux desktop <nhapeman@appliedinvention.com>
2019-12-02 11:07:51 -0800

    Lock down fps to 45

--- whirling/run_whirling.py
+++ whirling/run_whirling.py
@@ -10,7 +10,7 @@ from whirling.audio_visualizer import AudioVisualizer
 from whirling import audio_features
 from data.tracks import MUSIC_TRACKS
 
-DESIRED_FPS = 60
+DESIRED_FPS = 45
 
 
 ###############################################################################
