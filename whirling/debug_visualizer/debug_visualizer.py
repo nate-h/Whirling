97ce6a7b8407ff324b336b4d191d1b58a19a4ddc
nates linux desktop <nhapeman@appliedinvention.com>
2020-02-13 14:44:04 -0800

    Get whirling points to plot

--- whirling/debug_visualizer/debug_visualizer.py
+++ whirling/debug_visualizer/debug_visualizer.py
@@ -64,24 +64,17 @@ class DebugVisualizer(UIVisualizerBase):
 
 
             # # Convert beat events to frames and then plot them.
-            # for feature_name, data in framed_events.items():
-            #     beat_pnts = filter(
-            #         lambda x: min_window_frame <= x <= max_window_frame, data)
-            #     beat_pnts = [p - min_window_frame for p in beat_pnts]
-            #     pnts = np.zeros(max_window_frame - min_window_frame + 1)
-            #     np.put(pnts, beat_pnts, np.ones(len(beat_pnts)))
+            for feature_name, data in framed_events.items():
+                beat_pnts = filter(
+                    lambda x: min_window_frame <= x <= max_window_frame, data)
+                beat_pnts = [p - min_window_frame for p in beat_pnts]
+                pnts = np.zeros(max_window_frame - min_window_frame + 1)
+                np.put(pnts, beat_pnts, np.ones(len(beat_pnts)))
 
-            #     color = COLORS[row][1]
-
-            #     # Draw feature name text.
-            #     row_title(signal_name + ' - ' + feature_name)
-
-            #     # Draw points for subset of feature data.
-            #     for i, p in enumerate(pnts):
-            #         r = 0.004 if p > 0 else 0
-            #         point = create_point_from(i, p)
-            #         self.draw_circle(self.debug_surface, r, point, color)
-            #     row += 1
+                # Draw points for subset of feature data.
+                title = "%s - %s" % (signal_name, feature_name)
+                self.plot_discrete_signal(pnts, row_num, row_height, row_gap, title)
+                row_num += 1
 
         self.draw_time_indicator(curr_time, min_window_time, max_window_time)
 
@@ -121,19 +114,23 @@ class DebugVisualizer(UIVisualizerBase):
             glVertex2f(pl + x2, pb + y2*dh)
         glEnd()
 
-    def draw_line(self, point, size, color):
-        x,y = point
-        s = size/2.0
-        glVertex2f(x-s, y-s)      # bottom left point
-        glVertex2f(x+s, y-s)      # bottom right point
-        glVertex2f(x+s, y+s)      # top right point
-        glVertex2f(x-s, y+s)      # top left point
+    def plot_discrete_signal(self, points, row_num, row_height, row_gap, title):
 
-    def draw_rect(self, point, size, color):
-        glColor3f(*color)
-        x,y = point
-        s = size/2.0
-        glVertex2f(x-s, y-s)      # bottom left point
-        glVertex2f(x+s, y-s)      # bottom right point
-        glVertex2f(x+s, y+s)      # top right point
-        glVertex2f(x-s, y+s)      # top left point
+        dx = self.width/ len(points)
+        dh = row_height
+        pb = self.rect.bottom + row_num * (row_height+row_gap)
+        pl = self.rect.left
+        color = list(colors.COLORS.values())[row_num]
+
+        # TODO: not memoized!
+        pos = (pl, pb + dh + row_gap/10)
+        UIText(title, pos, font_size=20, font_color=color).draw()
+
+        # Draw checkerboard.
+        glPointSize(10.0)
+        glBegin(GL_POINTS)
+        glColor3fv(color)
+        for i, y1 in enumerate(points):
+            x1 = i*dx
+            glVertex2f(pl + x1, pb + y1*dh)
+        glEnd()
