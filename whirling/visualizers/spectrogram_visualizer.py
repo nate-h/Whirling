e899419e9bd23b035434a0b93c1ac652d7ba2c77
nates linux desktop <nhapeman@gmail.com>
2020-04-20 00:22:02 -0700

    Can see all spectrograms.

--- whirling/visualizers/spectrogram_visualizer.py
+++ whirling/visualizers/spectrogram_visualizer.py
@@ -22,44 +22,69 @@ class SpectrogramVisualizer(UIVisualizerBase):
         # What clip of full spectrogram we are looking at.
         self.spec_window = None
 
-        self.spec = None
+        self.specs = []
 
     def draw_visuals(self):
 
         curr_time = self.audio_controller.get_time()
 
         # Create spectrogram if it doesn't exist for this spec window.
-        self.create_spectrograms(curr_time)
+        if self.spec_window != math.floor(curr_time / self.seconds_worth):
+            self.create_spectrograms(curr_time)
 
-        if self.spec and self.spec.state == spectrogram.SpecState.LOADED:
-            self.spec.draw()
+        if len(self.specs): # and self.spec.state == spectrogram.SpecState.LOADED:
+            for s in self.specs:
+                s.draw()
 
         # Draw time indicator.
         self.draw_time_indicator(curr_time)
 
     def create_spectrograms(self, curr_time):
 
-        # Return if we've already generated the spectrograms for this window.
-        if self.spec_window == math.floor(curr_time / self.seconds_worth):
-            return
-
+        # Ditch old specs.
+        self.specs = []
 
         # Grab spectrogram and what portions we're going to limit it to.
-        D = self.data['full']['D']
+        num_rows = len(self.data)
+
+        if len(self.data) == 0:
+            logging.info('No spectrograms to view.')
+            return
+
+        if num_rows == 1:
+            row_padding = 0
+            row_height = self.height * 1.0 / num_rows
+        else:
+            row_padding = self.height * .1 / (num_rows - 1)
+            row_height = self.height * 0.9 / num_rows
+
+        print(self.height)
+        print(row_padding)
+        print(row_height)
+
         self.spec_window = math.floor(curr_time / self.seconds_worth)
         min_window_time = self.spec_window * self.seconds_worth
         max_window_time = (self.spec_window + 1) * self.seconds_worth
         min_window_frame = self.get_frame_number(min_window_time)
         max_window_frame = self.get_frame_number(max_window_time)
 
-        D_clip = D[:, min_window_frame: max_window_frame]
+        if max_window_frame == 0:
+            return
 
         t0 = time.time()
 
-        # Create the spectrogram.
-        top = self.height/3  + self.rect.bottom
-        spec_rect = Rect(self.rect.left, top, self.rect.right, self.rect.bottom)
-        self.spec = spectrogram.Spectrogram(spec_rect, D_clip)
+        # Create the spectrograms.
+        row_num = 0
+        for _signal_name, s_obj in self.data.items():
+            bottom = row_num * (row_padding + row_height) + self.rect.bottom
+            top = bottom + row_height
+            D = s_obj['D']
+            D_clip = D[:, min_window_frame: max_window_frame]
+            spec_rect = Rect(self.rect.left, top, self.rect.right, bottom)
+            spec = spectrogram.Spectrogram(spec_rect, D_clip)
+            self.specs.append(spec)
+            row_num += 1
+
 
         print(f'Total time: {time.time() - t0}')
 
