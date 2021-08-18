01d06f780db511864b5f9272c7a37146aff54981
nates linux desktop <nhapeman@gmail.com>
2021-08-17 22:59:00 -0700

    Lock dependencies of main app. (Not exploratory jupyter notebook dependencies)

--- setup.py
+++ setup.py
@@ -3,27 +3,27 @@ from setuptools import setup, find_packages
 
 setup(
     name='whirling',
-    version='0.0.3',
+    version='0.1.0',
     description='Audio visualizer using track segmentation and audio feature extraction',
     url='https://github.com/nate-h/Whirling',
     classifiers=[
-        'Programming Language :: Python :: 3.6.8',
+        'Programming Language :: Python :: 3.6.9',
     ],
     package_dir={'': '.'},
     packages=find_packages(),
     install_requires=[
-        'numpy',
+        'numpy==1.18.5',
         'pygame',
         'python-vlc',
-        'rx',
+        'Rx==3.2.0',
         'sklearn',
-        'librosa',
+        'librosa==0.8.0',
         'coloredlogs',
         'jupyter',
         'matplotlib',
-        'PyOpenGL',
-        'spleeter',
-        'schema'
+        'PyOpenGL==3.1.5',
+        'spleeter==2.2.2',
+        'schema==0.7.4'
     ],
     entry_points={
         'console_scripts': [
